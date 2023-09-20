03eda645ae2ff5103a8c6686be37e0a980c0af13
Irshad Hussain <razairshad04@gmail.com>
2023-09-20 12:10:27 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import todoapp from '../assets/projects/todoapp.png'
+import bgchanger from '../assets/projects/bgchanger.png'
+import OnlineExamination from '../assets/projects/OnlineExamination.png'
+import password from '../assets/projects/password.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React Next/JS TODO List",
+        image:todoapp,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Background-Changer React",
+        image:bgchanger,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"PHP Online Examination",
+        image:OnlineExamination,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Password Generator",
+        image:password,
+        github:"",
+        live:"",
+    },
+
+
+]
